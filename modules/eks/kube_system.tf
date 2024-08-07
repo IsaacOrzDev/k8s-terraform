ffe82f3165cbd40c15924da2481f4a03c9bdc8b5
Isaac <isaachkdeveloper@gmail.com>
2024-08-07 22:21:05 +0800

    fix: Resolve issue -- node had untolerated taint

--- modules/eks/kube_system.tf
+++ modules/eks/kube_system.tf
@@ -15,7 +15,7 @@ resource "aws_eks_fargate_profile" "kube-system" {
 
 }
 
-resource "null_resource" "k8s_patcher" {
+resource "null_resource" "k8s_coredns_patcher" {
   depends_on = [aws_eks_fargate_profile.kube-system]
 
   triggers = {
@@ -35,7 +35,49 @@ kubectl \
   --token="${data.aws_eks_cluster_auth.eks.token}" \
   patch deployment coredns \
   -n kube-system --type json \
-  -p='[{"op": "remove", "path": "/spec/template/metadata/annotations/eks.amazonaws.com~1compute-type"}]'
+  -p='[{"op": "add", "path": "/spec/template/spec/tolerations", "value": [
+      {
+        "key": "eks.amazonaws.com/compute-type",
+        "value": "fargate",
+        "effect": "NoSchedule"
+      }
+    ]}]'
+EOH
+  }
+
+  lifecycle {
+    ignore_changes = [triggers]
+  }
+}
+
+
+resource "null_resource" "k8s_external_dns_patcher" {
+  depends_on = [aws_eks_fargate_profile.kube-system]
+
+  triggers = {
+    endpoint = aws_eks_cluster.cluster.endpoint
+    ca_crt   = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+    token    = data.aws_eks_cluster_auth.eks.token
+  }
+
+  provisioner "local-exec" {
+    command = <<EOH
+cat >/tmp/ca.crt <<EOF
+${base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)}
+EOF
+kubectl \
+  --server="${aws_eks_cluster.cluster.endpoint}" \
+  --certificate_authority=/tmp/ca.crt \
+  --token="${data.aws_eks_cluster_auth.eks.token}" \
+  patch deployment external-dns \
+  -n kube-system --type json \
+  -p='[{"op": "add", "path": "/spec/template/spec/tolerations", "value": [
+      {
+        "key": "eks.amazonaws.com/compute-type",
+        "value": "fargate",
+        "effect": "NoSchedule"
+      }
+    ]}]'
 EOH
   }
 
